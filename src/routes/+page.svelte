4e967d8075c367642486262d3f307fbaaea8621e
Faizal Chan <moefc32@gmail.com>
2024-05-15 19:04:19 +0700

    backlog category loop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -89,135 +89,89 @@
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <main class="flex flex-row flex-1 mb-auto pt-3 bg-slate-50 w-full">
   <div class="flex flex-1 px-3 overflow-x-auto">
-    <div class="backlog-category flex flex-1 flex-col gap-2 px-2 border-r">
-      <div class="flex gap-1 leading-5 px-1 py-3 border-b">
-        <svg
-          class="w-5 h-5"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-width="2"
-            d="M9 8h10M9 12h10M9 16h10M4.99 8H5m-.02 4h.01m0 4H5"
-          />
-        </svg>
-        To-Do
-      </div>
-      <div
-        class="backlog-container flex flex-1 flex-col gap-1 overflow-y-auto px-1 pb-3"
-        on:dragover={(event) => event.preventDefault()}
-        on:drop={(event) => drop(event, "to_do")}
-      >
-        {#each backlogData.to_do as item, itemIndex (item)}
-          <div
-            class="backlog-item px-4 py-3 border rounded-lg bg-white w-full shadow-sm"
-            title={item.title}
-            on:click={() => openModalUpdate(item)}
-            draggable={true}
-            on:dragstart={(event) => dragStart(event, "to_do", itemIndex)}
-            animate:flip={{ duration: 250 }}
-          >
-            <div class="mb-1 text-lg line-clamp-2">{item.title}</div>
-            <div class="mb-2 text-xs text-gray-700">
-              {"Due " + datePrettier(item.due, false)}
-            </div>
-            {#if item.urgency === "low"}
-              <span class="badge variant-filled-surface text-xs">Low</span>
-            {:else if item.urgency === "medium"}
-              <span class="badge variant-filled-warning text-xs">Medium</span>
-            {:else}
-              <span class="badge variant-filled-error text-xs">Urgent</span>
-            {/if}
-          </div>
-        {/each}
-      </div>
-    </div>
-    <div class="backlog-category flex flex-1 flex-col gap-2 px-2 border-r">
-      <div class="flex gap-1 leading-5 px-1 py-3 border-b">
-        <svg
-          class="w-5 h-5"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="m16 10 3-3m0 0-3-3m3 3H5v3m3 4-3 3m0 0 3 3m-3-3h14v-3"
-          />
-        </svg>
-        In Progress
-      </div>
-      <div
-        class="backlog-container flex flex-1 flex-col gap-1 overflow-y-auto px-1 pb-3"
-        on:dragover={(event) => event.preventDefault()}
-        on:drop={(event) => drop(event, "in_progress")}
-      >
-        {#each backlogData.in_progress as item, itemIndex (item)}
-          <div
-            class="backlog-item px-4 py-3 border rounded-lg bg-white w-full shadow-sm"
-            title={item.title}
-            on:click={() => openModalUpdate(item)}
-            draggable={true}
-            on:dragstart={(event) => dragStart(event, "in_progress", itemIndex)}
-            animate:flip={{ duration: 250 }}
-          >
-            <div class="mb-1 text-lg line-clamp-2">{item.title}</div>
-            <div class="mb-2 text-xs text-gray-700">
-              {"Due " + datePrettier(item.due, false)}
-            </div>
-            {#if item.urgency === "low"}
-              <span class="badge variant-filled-surface text-xs">Low</span>
-            {:else if item.urgency === "medium"}
-              <span class="badge variant-filled-warning text-xs">Medium</span>
-            {:else}
-              <span class="badge variant-filled-error text-xs">Urgent</span>
-            {/if}
-          </div>
-        {/each}
-      </div>
-    </div>
-    {#if enableReview}
+    {#each Object.entries(backlogData) as [status, backlogs]}
       <div class="backlog-category flex flex-1 flex-col gap-2 px-2 border-r">
         <div class="flex gap-1 leading-5 px-1 py-3 border-b">
-          <svg
-            class="w-5 h-5"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-width="2"
-              d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"
-            />
-          </svg>
-          In Review
+          {#if status === "to_do"}
+            <svg
+              class="w-5 h-5"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-width="2"
+                d="M9 8h10M9 12h10M9 16h10M4.99 8H5m-.02 4h.01m0 4H5"
+              />
+            </svg> To-Do
+          {:else if status === "in_progress"}
+            <svg
+              class="w-5 h-5"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m16 10 3-3m0 0-3-3m3 3H5v3m3 4-3 3m0 0 3 3m-3-3h14v-3"
+              />
+            </svg> In Progress
+          {:else if status === "in_review"}
+            <svg
+              class="w-5 h-5"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-width="2"
+                d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"
+              />
+            </svg> In Review
+          {:else}
+            <svg
+              class="w-5 h-5"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M5 11.917 9.724 16.5 19 7.5"
+              />
+            </svg> Done
+          {/if}
         </div>
         <div
           class="backlog-container flex flex-1 flex-col gap-1 overflow-y-auto px-1 pb-3"
           on:dragover={(event) => event.preventDefault()}
-          on:drop={(event) => drop(event, "in_review")}
+          on:drop={(event) => drop(event, status)}
         >
-          {#each backlogData.in_review as item, itemIndex (item)}
+          {#each backlogs as item, itemIndex (item)}
             <div
               class="backlog-item px-4 py-3 border rounded-lg bg-white w-full shadow-sm"
               title={item.title}
               on:click={() => openModalUpdate(item)}
               draggable={true}
-              on:dragstart={(event) => dragStart(event, "in_review", itemIndex)}
+              on:dragstart={(event) => dragStart(event, status, itemIndex)}
               animate:flip={{ duration: 250 }}
             >
               <div class="mb-1 text-lg line-clamp-2">{item.title}</div>
@@ -235,56 +189,7 @@
           {/each}
         </div>
       </div>
-    {/if}
-    <div class="backlog-category flex flex-1 flex-col gap-2 px-2">
-      <div class="flex gap-1 leading-5 px-1 py-3 border-b">
-        <svg
-          class="w-5 h-5"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M5 11.917 9.724 16.5 19 7.5"
-          />
-        </svg>
-        Done
-      </div>
-      <div
-        class="backlog-container flex flex-1 flex-col gap-1 overflow-y-auto px-1 pb-3"
-        on:dragover={(event) => event.preventDefault()}
-        on:drop={(event) => drop(event, "done")}
-      >
-        {#each backlogData.done as item, itemIndex (item)}
-          <div
-            class="backlog-item px-4 py-3 border rounded-lg bg-white w-full shadow-sm"
-            title={item.title}
-            on:click={() => openModalUpdate(item)}
-            draggable={true}
-            on:dragstart={(event) => dragStart(event, "done", itemIndex)}
-            animate:flip={{ duration: 250 }}
-          >
-            <div class="mb-1 text-lg line-clamp-2">{item.title}</div>
-            <div class="mb-2 text-xs text-gray-700">
-              {"Due " + datePrettier(item.due, false)}
-            </div>
-            {#if item.urgency === "low"}
-              <span class="badge variant-filled-surface text-xs">Low</span>
-            {:else if item.urgency === "medium"}
-              <span class="badge variant-filled-warning text-xs">Medium</span>
-            {:else}
-              <span class="badge variant-filled-error text-xs">Urgent</span>
-            {/if}
-          </div>
-        {/each}
-      </div>
-    </div>
+    {/each}
   </div>
 </main>
 <Footer />
@@ -295,7 +200,7 @@
   }
 
   .backlog-category {
-    min-width: 200px;
+    min-width: 250px;
   }
 
   .backlog-container {
