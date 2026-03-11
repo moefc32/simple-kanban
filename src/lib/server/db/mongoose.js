c6a56db8fe513752963e517b7adb731cfcfe5f66
Faizal Chan. <moefc32@gmail.com>
2026-03-11 20:13:30 +0700

    mongoose optimizations

--- src/lib/server/db/mongoose.js
+++ src/lib/server/db/mongoose.js
@@ -1,15 +1,28 @@
 import { VITE_DATABASE_URL } from '$env/static/private';
-import { connect } from 'mongoose';
+import mongoose from 'mongoose';
 import Users from './model/users'
 
-let isConnected = 0;
+const { connect, connection } = mongoose;
+
+async function shutdown() {
+    console.log('\n--- Closing MongoDB Connection ---\n');
+
+    await connection.close();
+    process.exit(0);
+};
 
 export async function initMongoDB() {
-    if (isConnected) return;
+    if (connection.readyState === 1) return;
 
     try {
-        const db = await connect(VITE_DATABASE_URL);
-        isConnected = db.connections[0].readyState === 1;
+        const db = await connect(VITE_DATABASE_URL, {
+            maxPoolSize: 20,
+            minPoolSize: 2,
+            waitQueueTimeoutMS: 10 * 1000,
+            heartbeatFrequencyMS: 30 * 1000,
+        });
+
+        console.log('\n--- MongoDB Connected ---\n');
     } catch (e) {
         console.error('MongoDB connection error!', e);
     }
@@ -18,7 +31,11 @@ export async function initMongoDB() {
 export async function checkIsUserExists(id) {
     if (!id) return false;
 
-    const user = await Users.findById(id).lean();
+    const user = await Users.findById(id)
+        .select('_id').lean();
 
     return !!user;
 }
+
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
